d5e2f8b802fb3e7d9a365481f11488f899530ee3
Alexouuuu <alexchisiu@gmail.com>
2025-03-11 14:39:37 +0100

    vs

---
+++ Script.js
@@ -0,0 +1,24 @@
+let panier = JSON.parse(localStorage.getItem("panier")) || [];
+
+function ajouterAuPanier(nom,prix) {
+    panier.push({nom,prix});
+    localStorage.setItem("panier", JSON.stringify(panier));
+    mettreAJourPanier();
+}
+
+function mettreAJourPanier() {
+    let cartCount = document.getElementByID("cart-count");
+    cartCount.textContent = panier.length;
+}
+
+document.getElementById("theme-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    localStorage.setItem("theme", document.body.classList.contains("dark-mode") ? "dark" : "light");
+});
+
+window.onload = function() {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+    mettreAJourPanier();
+};
